09caf1a98c66963e4d9069a99f1968ce0188cb42
Roeck <rons9n@gmail.com>
2020-11-02 21:21:17 -0500

    set up first route

--- server/index.js
+++ server/index.js
@@ -5,19 +5,19 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
-const app = express();
+const app=express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({limit: '30mb',extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb',extended: true}))
 app.use(cors());
 
-app.use('/posts', postRoutes);
+const CONNECTION_URL='mongodb+srv://photomedia:RAMG9797@cluster0.ifp44.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT=process.env.PORT||5000;
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true,useUnifiedTopology: true})
+  .then(() => app.listen(PORT,() => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify',false);
