900bf3c5e75eace5a53885d9fb4c6645eac75213
Roeck <rons9n@gmail.com>
2020-11-13 19:27:51 -0500

    added card component details

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -10,7 +10,7 @@ const Post=({post}) => {
     const classes=useStyles();
     return (
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile||'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
@@ -21,23 +21,24 @@ const Post=({post}) => {
                 </Button>
             </div>
             <div className={classes.details}>
-                <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
-            <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
             <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+                <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => {}}>
                     <ThumbUpAltIcon fontSize="small" />
-                    Like
+                    LIKE
                     {post.likeCount}
                 </Button>
                 <Button size="small" color="primary" onClick={() => {}}>
                     <DeleteIcon fontSize="small" />
-                    Delete
+                    DELETE
                 </Button>
             </CardActions>
+
+
         </Card>
     );
 }
