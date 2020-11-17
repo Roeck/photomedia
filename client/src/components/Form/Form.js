4292fae5b0e9391912847c678ed320e1bda17a17
Roeck <rons9n@gmail.com>
2020-11-17 15:17:18 -0500

    fixed update functionality and mongoose request

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,14 @@
 import React,{useState,useEffect} from 'react';
 import {TextField,Button,Typography,Paper} from '@material-ui/core';
-import FileBase from 'react-file-base64';
 import {useDispatch,useSelector} from 'react-redux';
+import FileBase from 'react-file-base64';
 
 import useStyles from './styles';
 import {createPost,updatePost} from '../../actions/posts';
 
 const Form=({currentId,setCurrentId}) => {
     const [postData,setPostData]=useState({creator: '',title: '',message: '',tags: '',selectedFile: ''});
-    const post=useSelector((state) => (currentId? state.posts.find((p) => p._id===currentId):null));
+    const post=useSelector((state) => (currentId? state.posts.find((message) => message._id===currentId):null));
     const dispatch=useDispatch();
     const classes=useStyles();
 
@@ -16,25 +16,27 @@ const Form=({currentId,setCurrentId}) => {
         if(post) setPostData(post);
     },[post]);
 
+    const clear=() => {
+        setCurrentId(0);
+        setPostData({creator: '',title: '',message: '',tags: '',selectedFile: ''});
+    };
 
-    const handleSubmit=(e) => {
+    const handleSubmit=async (e) => {
         e.preventDefault();
 
-        if(currentId) {
-            dispatch(updatePost(currentId,postData));
-        } else {
+        if(currentId===0) {
             dispatch(createPost(postData));
+            clear();
+        } else {
+            dispatch(updatePost(currentId,postData));
+            clear();
         }
-    }
-
-    const clear=() => {
-
-    }
+    };
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Create your post</Typography>
+                <Typography variant="h6">{currentId? `Editing "${post.title}"`:'Creating a Memory'}</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({...postData,creator: e.target.value})} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({...postData,title: e.target.value})} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({...postData,message: e.target.value})} />
@@ -45,6 +47,6 @@ const Form=({currentId,setCurrentId}) => {
             </form>
         </Paper>
     );
-}
+};
 
 export default Form;
