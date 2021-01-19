cf8c0a8e8b5013906120f4181d0ee5a54158ce47
R B <rons9n@gmail.com>
2021-01-18 19:14:24 -0500

    NavBar as a new component

--- client/src/App.js
+++ client/src/App.js
@@ -19,12 +19,6 @@ const App = () => {
 
   return (
     <Container maxWidth='lg'>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.heading} variant='h2' align='center'>
-          PhotoMedia
-        </Typography>
-        <img className={classes.image} src={logo} alt='icon' height='60' />
-      </AppBar>
       <Grow in>
         <Container>
           <Grid
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { combineReducers } from 'redux';
+import { AppBar, Typography } from '@material-ui/core';
+
+const Navbar = () => {
+    return (
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+            <Typography className={classes.heading} variant='h2' align='center'>
+                 PhotoMedia
+            </Typography>
+            <img className={classes.image} src={logo} alt='icon' height='60' />
+        </AppBar>
+
+    )
+}
+
+export default Navbar;
