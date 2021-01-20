fa8eba57d6481becd5af59f46937161fe061402e
R B <rons9n@gmail.com>
2021-01-20 18:00:30 -0500

    moved app.js logics into new home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
 import Navbar from './components/Navbar/Navbar';
-import Form from './components/Form/Form';
-import useStyles from './styles';
+import Home from './components/Home/Home';
 
-const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
-
-  return ( 
+const App = () => (
     <Container maxWidth='lg'>
       <Navbar />
-    
+      <Home />
     </Container>
   );
-};
 
 export default App;
