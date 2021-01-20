d05f49f3e66431a5e58a2b93fc53fa437268b935
R B <rons9n@gmail.com>
2021-01-20 17:44:47 -0500

    toolbar component added to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
+import Navbar from './components/Navbar/Navbar';
 import Form from './components/Form/Form';
-import logo from './images/logo.png';
 import useStyles from './styles';
 
 const App = () => {
@@ -17,25 +18,10 @@ const App = () => {
     dispatch(getPosts());
   }, [dispatch]);
 
-  return (
+  return ( 
     <Container maxWidth='lg'>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justify='space-between'
-            alignItems='stretch'
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar />
+    
     </Container>
   );
 };
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React, { useDebugValue } from 'react';
+import React from 'react';
 import {
   Card,
   CardActions,
