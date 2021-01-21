55e80ed71f94d3fb1e5f82c29759b2cc4064dea0
R B <rons9n@gmail.com>
2021-01-21 18:05:34 -0500

    Sign up form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,8 +8,7 @@ import Input from './Input';
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
-
-    const isSignup = false;
+    const [isSignup, setIsSignup] = useState(false);
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
@@ -21,13 +20,18 @@ const Auth = () => {
 
     }
 
+    const switchMode = () => {
+        setIsSignup((prevSignup) => !prevSignup);
+        handleShowPassword(false);
+    }
+
     return (
         <Container component="main" maxWidth="xs">
             <Paper className={classes.paper} elevation={3}>
                 <Avatar className={classes.avatar}>
                     <LockOutlinedIcon />
                 </Avatar>
-                <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <Typography component="h1" variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
                 <form className={classes.form} onSubmit={handleSubmit}>
                     <Grid container spacing={2}>
                         {
@@ -44,6 +48,13 @@ const Auth = () => {
                     <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                         { isSignup ? 'Sign Up' : 'Sign In' }
                     </Button>
+                    <Grid container justify="flex-end">
+                       <Grid item>
+                           <Button onClick={switchMode}>
+                              { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+                           </Button>
+                       </Grid>
+                    </Grid>
                 </form>
             </Paper>
         </Container>
