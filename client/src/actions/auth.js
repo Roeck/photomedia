fd2f76852a4dbca6fd99df6702389f3ee24a625b
R B <rons9n@gmail.com>
2021-01-25 16:50:08 -0500

    JWT Login

---
+++ client/src/actions/auth.js
@@ -0,0 +1,23 @@
+import { AUTH } from '../constants/actionTypes';
+import * as api from '../api/index.js';
+
+export const signin = (formData, router) => async (dispatch) => {
+  try {
+    // log in the user
+
+    history.push('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, router) => async (dispatch) => {
+  try {
+    // sign up the user
+
+    history.push('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,22 +8,32 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Icon from './icon';
 import useStyles from './styles';
 import Input from './Input';
+import { signin, signup } from '../../actions/auth';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' }
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch();
     const history = useHistory();
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(isSignup) {
+            dispatch(signup(formData, history))
+        } else {
+            dispatch(signin(formData, history))
+        }
     };
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
     const switchMode = () => {
@@ -62,7 +72,7 @@ const Auth = () => {
                         isSignup && (
                         <>
                             <Input name="firstName" label="First Name" handleChange={handleChange} autofocus half />
-                            <Input name="firstName" label="First Name" handleChange={handleChange} half />
+                            <Input name="lastName" label="Last Name" handleChange={handleChange} half />
                         </>
                         )}
                         <Input name="email" label="Email address" handleChange={handleChange} type="email" />
