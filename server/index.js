355539d158cfbce2cea89022533fc089854d4a09
R B <rons9n@gmail.com>
2021-01-27 19:01:55 -0500

    added build script

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 const CONNECTION_URL =
-  'mongodb+srv://photomedia:RAMG9797@cluster0.ifp44.mongodb.net/<dbname>?retryWrites=true&w=majority';
+  'mongodb+srv://rons9n:ramg999@cluster0.ifp44.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
